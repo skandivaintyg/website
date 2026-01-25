af9ab2c8ddae277a4ac5e8e3ad45d5d9840657aa
Skandiva Intyg <tri@skandivaintyg.se>
2026-01-25 16:23:09 -0500

    Fix site config bootstrap

--- includes/config.php
+++ includes/config.php
@@ -1,11 +1,13 @@
-  "name" => "Skandiva Intyg",
-  "phone" => "0700-23-00-23",
-  "email" => "hej@skadivaityg.se",
+<?php
+// includes/config.php
 
-
-$SITE = [
-  "name" => "Medicinska intyg Sverige",
-  "phone" => "08-512 588 00",
+$SITE = [
+  "name" => "Medicinska intyg Sverige",
+  "phone" => "08-512 588 00",
+  "email" => "info@dinmottagning.se",
+  "lang" => "sv",
+];
+
   "email" => "info@dinmottagning.se",
   "lang" => "sv",
 ];
